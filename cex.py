9e03a74c6da445fdc912ad88e187efb69df3546a
tpk@tb3 <tpkludka@gmail.com>
2017-06-14 15:24:03 +0100

    works on local machine

--- cex.py
+++ cex.py
@@ -5,10 +5,11 @@ import os
 def stamp_it():
     dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     # fpath = os.path.join(os.path.expanduser('~'),'project/cron/cronlog.txt')
-    froot = '/home/tpk/project/cron/'
+    froot = '/home/tpk/projects/cron/'
     fpath = froot + 'cronlog' + datetime.now().strftime('%Y%m%d%H%M%S') + '.txt'
     f = open(fpath, "a+")
     f.write("Script executed @ "+dt+"\n")
-    
+    f.close
+
 if __name__ == '__main__':
     stamp_it()
