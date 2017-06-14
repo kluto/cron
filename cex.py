6353bc936133ad0cb7f9816ccb79173dc0d8246c
tpk@tb1 <tpkludka@gmail.com>
2017-06-14 14:30:16 +0100

    first example not working on vps

---
+++ cex.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+import os
+
+def stamp_it():
+    dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # fpath = os.path.join(os.path.expanduser('~'),'project/cron/cronlog.txt')
+    froot = '/home/tpk/project/cron/'
+    fpath = froot + 'cronlog' + datetime.now().strftime('%Y%m%d%H%M%S') + '.txt'
+    f = open(fpath, "a+")
+    f.write("Script executed @ "+dt+"\n")
+    
+if __name__ == '__main__':
+    stamp_it()
---
+++ readme.txt
@@ -0,0 +1,12 @@
+# .---------------- minute (0 - 59)
+# |  .------------- hour (0 - 23)
+# |  |  .---------- day of month (1 - 31)
+# |  |  |  .------- month (1 - 12) OR jan,feb,mar,apr ...
+# |  |  |  |  .---- day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue,wed,thu,fri,sat
+# |  |  |  |  |
+# *  *  *  *  * user-name  command to be executed
+
+# Every 5 minutes
+# */5 * * * * ~/my_script.sh
+# or
+# 0,5,10,15,20,25,30,35,40,45,50,55 * * * * ~/my_script.sh
